acd2ab53eabdc8e848b11e79a21a9e0be631596d
Shayla Nguyen <itsshaylan@gmail.com>
2021-06-03 09:46:12 -0500

    new js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,10 @@
 jQuery(function() {
-
-//    jQuery("#fittext1").fitText().fitText(5);
-    jQuery("#text-div1 p").fitText().fitText(5.55);
-    jQuery("#text-div2 p").fitText().fitText(3.55);
-    jQuery("#text-div3 p").fitText().fitText(3.55);
-    jQuery("#text-div4 p").fitText().fitText(3.55);
-    jQuery("#text-div5 p").fitText().fitText(3.55);
-    jQuery(".headline").fitText().fitText(1.42);
-    jQuery(".mainhead").fitText().fitText(1.399);
-   
+    jQuery("#text-div1 p").fitText().fitText(4.55);
+    jQuery("#text-div2 p").fitText().fitText(4.1);
+    jQuery("#text-div3 p").fitText().fitText(4.1);
+    jQuery("#text-div4 p").fitText().fitText(4.1);
+    jQuery("#text-div5 p").fitText().fitText(4.1);
+    jQuery(".headline").fitText().fitText(1.7);
+    jQuery(".mainhead").fitText().fitText(1.72);   
 }); 
 
